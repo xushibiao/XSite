e0563956eec8711643cd7cb1dd83aae8b79839a8
xu <919469733@qq.com>
2020-04-24 03:23:10 +0800

    服务器版本发布

--- XSite/settings.py
+++ XSite/settings.py
@@ -98,7 +98,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
     'article',
     'user',
     'error_page',
